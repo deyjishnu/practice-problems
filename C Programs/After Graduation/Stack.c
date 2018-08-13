062e587b3d8dc39a17578395d0ac37cf8cd98b2d
Jishnu Dey <incredible.jishnu@gmail.com>
2018-08-14 03:15:43 +0530

    Edit two C files and delete an unnecessary C file

--- C Programs/After Graduation/Stack.c
+++ C Programs/After Graduation/Stack.c
@@ -1,22 +1,57 @@
+// Stack in C
 #include <stdio.h>
+#include <stdlib.h>
 
-void push(int[], int item);
-int pop(int[]);
+void push(int[], int size, int item);
+int pop(int[], int size);
+void display(int[]);
+int top = -1;
 
 void main() {
-    int top, item, ch = 1, a[5];
-    do {
-        printf("Enter your choice:\n");
+    int top, item, ch = 1, size = 5, a[size];
+    while(1) {
+        printf("\nEnter your choice:\n");
         printf("1.PUSH 2.POP 3.DISPLAY 4.EXIT\n");
         scanf("%d", &ch);
-        switch {
+        switch(ch) {
             case 1: printf("Enter number to be pushed: ");
                     scanf("%d", &item);
-                    push(a, item);
+                    push(a, size, item);
                     break;
                     
-            case 2: 
+            case 2: printf("\nPopped --> %d", pop(a, size));
+                    break;
+
+            case 3: display(a);
+                    break;
+
+            case 4: exit(0);
+            default: printf("\nWrong entry\n");
         }
     }
-    while(ch != 0);
+}
+
+void push(int a[], int size, int item) {
+    if(top == size - 1)
+        printf("Stack full. You should pop first");
+    else
+        a[++top] = item;
+}
+
+int pop (int a[], int size) {
+    if(top == -1) {
+        printf("Stack empty");
+        return 0;
+    }
+    else
+        return a[top--];
+}
+
+void display(int a[]) {
+    printf("\nStack --> ");
+    int i;
+    for(i=0; i<=top;i++)
+        printf("%d ", a[i]);
+    if(top == -1)
+        printf("Stack is empty");
 }
--- C Programs/After Graduation/String.c
+++ C Programs/After Graduation/String.c
@@ -8,8 +8,8 @@ void main() {
 
     // CHARACTER ARRAYS IN C ARE CALLED STRINGS
     char s[10] = "Jishnu";
-    s[0] = 'V';
     printf("Size of s is %d\n", sizeof(s));
+    // char s[] = "Jishnu"; // You can also create a string without specifying its length
 
     // A CHARACTER ARRAY CONTAINS A NULL CHARACTER AT THE END
     int i;
@@ -17,18 +17,19 @@ void main() {
     printf("No. of characters in s = %d\n", i + 1); // Value printed will be one more than the number of characters stored in s
                                                     // because at the end, there is an additional null character "\0"
     puts(s); // Prints the string until null termination
+    printf("%s", s); // Another way to print string
 
     // COPYING CHARACTER ARRAYS
     char s2[10];
     // s2 = s; // This statement will give an error, because s2 is a constant pointer to the base address of an array
     // s2 = "Jishnu"; // This statement will give an error, because s2 is a constant pointer to the base address of an array
     strcpy(s2, s); // Best way to copy one character array to another. This function is found in string.h header file
-    s2[2] = s[2]; // This works because we can always change values of individual elements in an array
-    puts(s); // This function and all other functions which takes a character array prints values of the array until null termination
+    s2[0] = 'V'; // This works because we can always change values of individual elements in an array
+    puts(s2); // This function and all other functions which takes a character array prints values of the array until null termination
 
     // CHARACTER POINTER
     char* s3;
-    s3 = s; // This works because s3 is a pointer variable, and not a constant
+    s3 = s; // This works because s3 is a pointer variable, and not a constant. But this is not copying; s3 and s points to the same array
     puts(s3); // This works because puts() (and all other functions which takes a character array, 
               // even user-defined functions: see printString() method here) expects a character pointer.
               // Its declaration is: int puts(const char *str)
